6eac6a3acdddfaa4c522b0031bc10def78151c91
Natalie Perrochon <nperroch@uci.edu>
2026-05-15 00:31:55 -0700

    Move location of loader import

--- onpolicy/scripts/train/train_hanabi_forward.py
+++ onpolicy/scripts/train/train_hanabi_forward.py
@@ -26,21 +26,6 @@ print("[MAIN] imported transformers", flush=True)
 
 """Train script for Hanabi."""
 
-if (
-    getattr(all_args, "use_llm", False)
-    and getattr(all_args, "llm_vector_mode", "") == "analysis_embedding"
-):
-    print("[MAIN] preloading BERT text encoder before env worker fork", flush=True)
-
-    from onpolicy.envs.hanabi.llm_action_helper import preload_text_analysis_encoder
-
-    preload_text_analysis_encoder(
-        model_name="prajjwal1/bert-tiny",
-        device="cpu",
-    )
-
-    print("[MAIN] finished preloading BERT text encoder", flush=True)
-
 
 def make_train_env(all_args):
     def get_env_fn(rank):
@@ -245,6 +230,24 @@ def main(args):
                 old_len, all_args.episode_length
             )
         )
+        if (
+            getattr(all_args, "use_llm", False)
+            and getattr(all_args, "llm_vector_mode", "") == "analysis_embedding"
+        ):
+            print(
+                "[MAIN] preloading BERT text encoder before env worker fork", flush=True
+            )
+
+            from onpolicy.envs.hanabi.llm_action_helper import (
+                preload_text_analysis_encoder,
+            )
+
+            preload_text_analysis_encoder(
+                model_name="prajjwal1/bert-tiny",
+                device="cpu",
+            )
+
+            print("[MAIN] finished preloading BERT text encoder", flush=True)
 
     # env init
     envs = make_train_env(all_args)
