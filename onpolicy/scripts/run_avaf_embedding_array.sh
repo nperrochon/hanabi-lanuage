1ccba6d426a8167bdaab24bfbe32a68fdabe07fe
Natalie Perrochon <nperroch@uci.edu>
2026-06-02 21:54:24 -0700

    Remove wandbname from input script

--- onpolicy/scripts/run_avaf_embedding_array.sh
+++ onpolicy/scripts/run_avaf_embedding_array.sh
@@ -145,7 +145,6 @@ CMD=(
   --algorithm_name "${algo}"
   --experiment_name "${exp}"
   --hanabi_name "${hanabi}"
-  --user_name nperroch-uci
   --num_agents "${num_agents}"
   --seed "${seed}"
   --n_training_threads 1
