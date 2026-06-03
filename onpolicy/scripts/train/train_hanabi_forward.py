ed3ccb0221142a5582cd1342e0bb192171c49992
Natalie Perrochon <nperroch@uci.edu>
2026-06-02 21:58:35 -0700

    removing debug prints from training file

--- onpolicy/scripts/train/train_hanabi_forward.py
+++ onpolicy/scripts/train/train_hanabi_forward.py
@@ -176,13 +176,6 @@ def main(args):
     print(f"[DEBUG] run_dir={run_dir}", flush=True)
 
     if all_args.use_wandb:
-        print("[DEBUG] about to call wandb.init", flush=True)
-        run = wandb.init(...)
-        print(f"[DEBUG] wandb.init returned: {run}", flush=True)
-    else:
-        print("[DEBUG] NOT calling wandb.init because use_wandb is False", flush=True)
-
-    if all_args.use_wandb:
         run = wandb.init(
             config=all_args,
             project=all_args.env_name,
